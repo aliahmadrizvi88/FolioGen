3c1af4b63ba0f0613860b53ac7e0840cfd52523b
Ali Ahmad <aliahmadrizvi88@gmail.com>
2026-02-16 11:59:32 +0500

    frist commit

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+
+import LandingPage from '../pages/LandingPage';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
